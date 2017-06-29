265c6201b66b4cf5d7ffdbb3786ad35db60cb7ca
Simon Asika <asika32764@gmail.com>
2017-06-30 00:14:36 +0800

    Update run_editor.js

--- PhpStorm Protocol (Win)/run_editor.js
+++ PhpStorm Protocol (Win)/run_editor.js
@@ -3,10 +3,10 @@ var settings = {
 	x64: true,
 
 	// Set to folder name, where PhpStorm was installed to (e.g. 'PhpStorm')
-	folder_name: 'PhpStorm 2016.3',
+	folder_name: 'PhpStorm 2017.1.4',
 
 	// Set to window title (only text after dash sign), that you see, when switching to running PhpStorm instance
-	window_title: 'PhpStorm 2016.3',
+	window_title: 'PhpStorm 2017.1.4',
 
 	// In case your file is mapped via a network share and paths do not match.
 	// eg. /var/www will can replaced with Y:/
