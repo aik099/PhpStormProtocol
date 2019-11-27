256ecd52b6c315623b24243a8db3c3de3e083a88
Falko Hilbert <fhilbert@viosys.com>
2019-11-27 12:07:43 +0100

    add support for Jetbrains Toolbox

--- PhpStorm Protocol (Win)/run_editor.js
+++ PhpStorm Protocol (Win)/run_editor.js
@@ -1,4 +1,7 @@
 var settings = {
+	// flag to active Jetbrain Toolbox configuration
+	toolBoxActive: false,
+
 	// Set to 'true' (without quotes) if run on Windows 64bit. Set to 'false' (without quotes) otherwise.
 	x64: true,
 
@@ -20,6 +23,11 @@ var	url = WScript.Arguments(0),
 	match = /^phpstorm:\/\/open\/?\?(url=file:\/\/|file=)(.+)&line=(\d+)$/.exec(url),
 	project = '',
 	editor = '"C:\\' + (settings.x64 ? 'Program Files' : 'Program Files (x86)') + '\\JetBrains\\' + settings.folder_name + (settings.x64 ? '\\bin\\phpstorm64.exe' : '\\bin\\phpstorm.exe')+'"';
+
+if( settings.toolBoxActive )
+{
+	configureToolboxSettings(settings);
+}
 
 if (match) {
 
@@ -54,4 +62,56 @@ if (match) {
 
 	shell.Exec(command);
 	shell.AppActivate(settings.window_title);
+}
+
+function configureToolboxSettings(settings) {
+	var shell = new ActiveXObject('WScript.Shell'),
+		appDataLocal = shell.ExpandEnvironmentStrings("%localappdata%"),
+		toolboxDirectory = appDataLocal + '\\JetBrains\\Toolbox\\apps\\PhpStorm\\ch-0\\';
+
+	// Reference the FileSystemObject
+	var fso = new ActiveXObject('Scripting.FileSystemObject');
+
+	// Reference the Text directory
+	var folder = fso.GetFolder(toolboxDirectory);
+
+	// Reference the File collection of the Text directory
+	var fileCollection = folder.SubFolders;
+
+
+	var maxMajor = 0,
+		maxMinor = 0,
+		maxPatch = 0,
+		maxVersionFolder = "";
+	// Traverse through the fileCollection using the FOR loop
+	// read the maximum version from toolbox filesystem
+	for (var objEnum = new Enumerator(fileCollection); !objEnum.atEnd(); objEnum.moveNext()) {
+		var folderObject = ( objEnum.item() );
+		if (folderObject.Name.lastIndexOf('plugins') == -1) {
+			var versionMatch = /(\d+)\.(\d+)\.(\d+)/.exec(folderObject.Name),
+				major = parseInt(versionMatch[ 1 ]),
+				minor = parseInt(versionMatch[ 2 ]),
+				patch = parseInt(versionMatch[ 3 ])
+			if (maxMajor == 0 || maxMajor <= major) {
+				maxMajor = major;
+				if (maxMinor == 0 || maxMinor <= minor) {
+					maxMinor = minor;
+					if (maxPatch == 0 || maxPatch <= patch) {
+						maxPatch = patch;
+						maxVersionFolder = folderObject.Name;
+					}
+				}
+			}
+		}
+	}
+
+	settings.folder_name = maxVersionFolder;
+
+	// read version name and product name from product-info.json
+	var versionFile = fso.OpenTextFile(toolboxDirectory + settings.folder_name + "\\product-info.json", 1, true);
+	var content = versionFile.ReadAll();
+
+	eval('var productVersion = ' + content + ';');
+	settings.window_title = 'PhpStorm ' + productVersion.version;
+	editor = '"' + toolboxDirectory + settings.folder_name + '\\' + productVersion.launch[ 0 ].launcherPath.replace(/\//g, '\\') + '"';
 }
